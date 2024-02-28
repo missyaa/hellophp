17dea9128eaca749f3ce824d7805e658adf3f331
missyaa <110535206+missyaa@users.noreply.github.com>
2024-02-28 21:11:06 +1100

    Add files via upload

---
+++ hellophp/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <link href="style.css" type="text/css" rel="stylesheet">
+    </head>
+
+    <body>
+        <h1 class="realife">hello php - hip hip hooray, handsfree go for your life!</h1>
+
+        <h1>text</h1>
+
+        <?php echo "How the fuck is that happening?" ?>
+
+        <?php
+        $pish = "pusspuss";
+        $bilbo = "puppy";
+
+        ?>
+
+        <h1>
+            <?php echo $bilbo; ?>
+        </h1>
+
+    </body>
+
+</html>
+
+<!-- this is a comment -->
+
